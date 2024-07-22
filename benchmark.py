ca0d56fe9fe3240b259975066e8b79537291f0cf
Kunam Balaram Reddy <webbdays@gmail.com>
2024-07-22 09:02:56 +0530

    final and last and final

--- benchmark.py
+++ benchmark.py
@@ -36,7 +36,7 @@ for benchmark in config["benchmarks"].keys():
   
   
   ## start benchmark candidate server
-  server_start_command_script = "graphql/" + benchmark_candidate + "/run.sh";
+  server_start_command_script = "graphql/" + benchmark_candidate + "/run.sh &";
   subprocess.call(server_start_command_script);
 
   ## warmup the server
